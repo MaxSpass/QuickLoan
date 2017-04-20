7b491b4f47698e9316fbc01c8450202a2596d4b7
Max Spassky <maxspas1@gmail.com>
2017-04-20 23:10:58 +0300

    validation.js 5%

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,78 @@
 $(document).ready(function(){
-	// $('.hot-to-get label').each(function(){
-	// 	var i = 1;
-	// 	$(this).attr
-	// })
-})
+	$('.field-value').each(function(){
+		$('<span class="validation"></span>').appendTo($(this))
+	})
+
+function customValidate(id)
+{
+    var regex = /^[a-zA-Z ]{2,30}$/;
+    var ctrl =  document.getElemetnById(id);
+
+    if (regex.test(ctrl.value)) {
+        return true;
+        console.log('ok')
+    }
+    else {
+        return false;
+        console.log('xyu')
+    }
+}
+
+
+
+	$("#personal_info").validate({
+		debug: true,
+		// errorElement: 'span',
+		focusInvalid: true,
+		rules: {
+			lname: {
+				required: true
+				// minlength: 6,
+				// pattern: "[A-Za-zА-Яа-яЁё]"
+				
+				// submitHandler: function(){
+				// 	console.log('Имя введено')
+				// }
+			},
+			fname: {
+				required: true
+			},
+			pname: {
+				required: true
+			},
+			datebirth: {
+				required: true
+			},
+			telnum: {
+				required: true
+			},
+			email: {
+				required: true,
+				submitHandler: function(){
+					// this.parent().find('.validation').addClass('valid');
+					console.log('1')
+				}
+				// ,
+				// errorPlacement: function(error, element){
+				// 	error.appendTo(element);
+				// }
+			},
+		},
+		messages: {
+			lname: "Введите фамилию корретно",
+			fname: "Введите имя корретно",
+			pname: "Введите отчество корретно",
+			datebirth: "Введите дату рождения",
+			telnum: "Введите номер телефона",
+			email: "Введите Email корретно"
+		},
+		submitHandler: function() {
+			// $("body").css('background','red')
+			console.log('Всё прошло успешно')
+		}
+	});
+
+	// $('.datepicker').datepicker({
+	// 	minViewMode: 2
+	// });
+});
